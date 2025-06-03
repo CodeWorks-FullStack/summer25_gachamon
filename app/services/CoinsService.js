c122eca9f7ad7a58141d15700c041362b77927ce
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-03 10:01:27 -0600

    coins go up!

---
+++ app/services/CoinsService.js
@@ -0,0 +1,14 @@
+import { AppState } from "../AppState.js";
+
+class CoinsService {
+  increaseCoins() {
+    // NOTE stupid primitives
+    // let coins = AppState.coins
+    // coins++
+    AppState.coins++
+    console.log('increasing coins', AppState.coins);
+  }
+}
+
+// singleton
+export const coinsService = new CoinsService()
